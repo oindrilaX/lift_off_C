7b79282e5ff24f75ac7f2d70e30c171214760851
oindrilaX <127420955+oindrilaX@users.noreply.github.com>
2023-03-09 21:17:53 +0530

    Add files via upload

---
+++ week1_code3.c
@@ -0,0 +1,19 @@
+//  Write a program that takes a number as input and prints the sum of its digits using a do-while loop.
+#include <stdio.h>
+
+int main() {
+    int num, digit, sum = 0;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    do {
+        digit = num % 10;
+        sum += digit;     
+        num /= 10;        
+    } while (num != 0);
+    
+    printf("The sum of the digits is %d\n", sum);
+    
+    return 0;
+}
